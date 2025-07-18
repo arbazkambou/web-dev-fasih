250f5a804ec7833d62d6aa6ba295346e1265492d
Arbaz Shoukat <arbazkamboh342@.com>
2025-07-18 16:29:55 +0500

    usePopcorn component compostion

--- usePopcorn/src/App.jsx
+++ usePopcorn/src/App.jsx
@@ -1,11 +1,35 @@
+import { Children, useState } from "react";
 import Main from "./components/Main";
 import Navbar from "./components/Navbar";
+import { tempMovieData, tempWatchedData } from "./data/data";
+import Logo from "./components/Logo";
+import SearchInput from "./components/SearchInput";
+import WatchedBox from "./components/WatchedBox";
+import SearchResult from "./components/SearchResult";
+import MoviesList from "./components/MoviesList";
+import Box from "./components/Box";
+import WatchedSummary from "./components/WatchedSummary";
+import WatchedMoviesList from "./components/WatchedMoviesList";
 
 export default function App() {
+  const [movies, setMovies] = useState(tempMovieData);
+  const [watched, setWatched] = useState(tempWatchedData);
   return (
     <>
-      <Navbar />
-      <Main />
+      <Navbar>
+        <Logo />
+        <SearchInput />
+        <SearchResult movies={movies} />
+      </Navbar>
+      <Main>
+        <Box>
+          <MoviesList movies={movies} />
+        </Box>
+        <Box>
+          <WatchedSummary watched={watched} />
+          <WatchedMoviesList watched={watched} />
+        </Box>
+      </Main>
     </>
   );
 }
--- usePopcorn/src/components/Navbar.jsx
+++ usePopcorn/src/components/Navbar.jsx
@@ -1,25 +1,5 @@
-import { useState } from "react";
-
-function Navbar() {
-  const [query, setQuery] = useState("");
-  return (
-    <nav className="nav-bar">
-      <div className="logo">
-        <span role="img">🍿</span>
-        <h1>usePopcorn</h1>
-      </div>
-      <input
-        className="search"
-        type="text"
-        placeholder="Search movies..."
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-      />
-      <p className="num-results">
-        Found <strong>x</strong> results
-      </p>
-    </nav>
-  );
+function Navbar({ children }) {
+  return <nav className="nav-bar">{children}</nav>;
 }
 
 export default Navbar;
